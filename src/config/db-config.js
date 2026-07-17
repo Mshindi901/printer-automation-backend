39253d111b0c62b5e015f0c5be7b5f44b8e6d160
Mshindi901 <emmanuelmshindi68@gmail.com>
2026-07-17 08:56:35 -0400

    added a database url

--- src/config/db-config.js
+++ src/config/db-config.js
@@ -1,6 +1,6 @@
 import Sequelize from "sequelize";
 
-const sequelize = new Sequelize('printer', 'postgres', 'Yugah2005@', {
+const sequelize = new Sequelize('postgresql://test_v4w1_user:6HvjFE3jioxXJfsNG2LxWyRKI7vKBTno@dpg-d9d2c8t7vvec73enebhg-a/test_v4w1', {
     host: 'localhost',
     dialect: 'postgres',
     logging: false
