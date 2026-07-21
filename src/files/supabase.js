5539b41bb71b59cc2bffc3711d833823844e8c44
Mshindi901 <emmanuelmshindi68@gmail.com>
2026-07-21 16:37:52 +0300

    added a websocket feature for the VPS

--- src/files/supabase.js
+++ src/files/supabase.js
@@ -1,7 +1,15 @@
 import {createClient} from '@supabase/supabase-js';
 import dotenv from 'dotenv';
+import WebSocket from 'ws';
 dotenv.config();
 
-const supabase = createClient(process.env.SUPABASE_PROJECT_URL,process.env.SUPABASE_SERVICE_KEY);
+const supabase = createClient(process.env.SUPABASE_PROJECT_URL,process.env.SUPABASE_SERVICE_KEY, {
+    auth:{
+        persistSession:false
+    },
+    realtime:{
+        transport: WebSocket
+    }
+});
 
 export default supabase;
